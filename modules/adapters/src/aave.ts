7e31172dc806263cd8b09f5bfb725b634d80779e
bohendo <bohendo@pm.me>
2021-06-19 08:57:46 +0530

    add a market address to aave

--- modules/adapters/src/aave.ts
+++ modules/adapters/src/aave.ts
@@ -18,13 +18,19 @@ export const aaveSource = "Aave";
 const v1MarketAddresses = [
   { name: "aETH", address: "0x3a3a65aab0dd2a17e3f1947ba16138cd37d08c04" },
   { name: "aDAI", address: "0xfc1e690f61efd961294b3e1ce3313fbd8aa4f85d" },
-  // TODO: add more Aave contract addresses
+  // TODO: add some more Aave contract addresses
   // v1: https://docs.aave.com/developers/v/1.0/deployed-contracts/deployed-contract-instances
   // v2: https://docs.aave.com/developers/deployed-contracts/deployed-contracts
 ].map(row => ({ ...row, category: AddressCategories.Defi })) as AddressBookJson;
 
+const v2MarketAddresses = [
+  { name: "aDAI", address: "0x028171bca77440897b824ca71d1c56cac55b68a3" },
+].map(row => ({ ...row, category: AddressCategories.Defi })) as AddressBookJson;
+
+
 export const aaveAddresses = [
   ...v1MarketAddresses,
+  ...v2MarketAddresses,
 ];
 
 ////////////////////////////////////////
