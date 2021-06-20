bc10c9da3ad8a4dea0b249432f90ab73e7068df9
bohendo <bohendo@pm.me>
2021-06-20 18:41:13 +0530

    upgrade valuemachine to 0.2.0

--- modules/server/ops/entry.sh
+++ modules/server/ops/entry.sh
@@ -13,10 +13,10 @@ fi
 
 if [[ "$DEGENFOLIO_PROD" == "true" ]]
 then
-  echo "Starting valuemachine server in prod-mode"
+  echo "Starting degenfolio server in prod-mode"
   exec node --no-deprecation dist/bundle.js
 else
-  echo "Starting valuemachine server in dev-mode"
+  echo "Starting degenfolio server in dev-mode"
   exec ./node_modules/.bin/nodemon \
     --delay 1 \
     --exitcrash \
