93617afc1a8f749fbf10fc2ada162cff2dc92358
Dhruv035 <myemail@abc.com>
2021-06-24 09:17:17 +0530

    basic_api_fetch

--- modules/adapters/src/harmony/harmonydata.ts
+++ modules/adapters/src/harmony/harmonydata.ts
@@ -1,1 +1,22 @@
 //edit to check ssh connnection
+import axios from "axios";
+async function getHarmony() {
+  const databc = {
+    jsonrpc: "2.0",
+    id: 1,
+    method: "hmyv2_getTransactionsHistory",
+    params: [
+      {
+        address: "one1rvaqpfukjsxz5gaqtjr8hz9mtqevr9p4gfuncs",
+        pageIndex: 0,
+        pageSize: 20,
+        fullTx: true,
+        txType: "ALL",
+        order: "ASC"
+      }
+    ]
+  };
+  const response = await axios.post("https://rpc.s0.b.hmny.io", databc);
+  console.log(response.data);
+}
+getHarmony();
