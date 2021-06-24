fb57c57e7ecd2c79b6d5c493334770e423dc6d3b
shivani <gupta.shivani254@gmail.com>
2021-06-24 19:26:18 +0530

    🚨 rm some warnings

--- modules/client/src/components/Portfolio.tsx
+++ modules/client/src/components/Portfolio.tsx
@@ -1,36 +1,34 @@
 import React, { useState, useContext } from "react";
 import { useEffect } from "react";
-import {XYPlot, VerticalRectSeries, XAxis, RVTickFormat } from 'react-vis';
+import { XYPlot, VerticalRectSeries, XAxis, RVTickFormat } from "react-vis";
 import { timeFormat } from "d3-time-format";
 
 import { AccountContext } from "./AccountManager";
 
-const sampleData = [ {x: 1, y: 208}, {x: 5, y: 5890}, ];
-
 const getPriceCurrent = (asset: string) => {
   switch (asset) {
-    case "DAI": return 1;
-    case "ETH": return 2400;
-    case "WBTC": return 35000;
-    case "UniV2_WBTC_ETH": return 2700000000;
-    default: return 1;
+  case "DAI": return 1;
+  case "ETH": return 2400;
+  case "WBTC": return 35000;
+  case "UniV2_WBTC_ETH": return 2700000000;
+  default: return 1;
   }
-}
+};
 
 const getPriceHistorical = (asset: string) => {
   switch (asset) {
-    case "DAI": return 1;
-    case "ETH": return 400;
-    case "WBTC": return 3500;
-    case "UniV2_WBTC_ETH": return 27000000;
-    default: return 1;
+  case "DAI": return 1;
+  case "ETH": return 400;
+  case "WBTC": return 3500;
+  case "UniV2_WBTC_ETH": return 27000000;
+  default: return 1;
   }
-}
+};
 
 export const Portfolio = () => {
   const { vm } = useContext(AccountContext);
 
-  const [data, setData] = useState([] as Array<{x: number, y: number, x0: number, y0: number}>)
+  const [data, setData] = useState([] as Array<{x: number, y: number, x0: number, y0: number}>);
   const locale = timeFormat("%m/%y");
 
   const formatChunksToGraphData = () => {
@@ -39,17 +37,21 @@ export const Portfolio = () => {
 
     const newData = [] as Array<{x: number, y: number, x0: number, y0: number}>;
     
-    chunks.forEach((value, index) => {
+    chunks.forEach((value) => {
       const currentDate = new Date();
       const quantity = parseFloat(value.quantity) ;
       const disposeDate = value.disposeDate
         ? new Date(value.disposeDate).getTime()
-        : currentDate.getTime()
+        : currentDate.getTime();
         
       console.log(
         value.asset,
-        {x: new Date(value.receiveDate).getTime(), y: quantity*getPriceHistorical(value.asset)},
-        {x: disposeDate, y: quantity*getPriceCurrent(value.asset)}
+        {
+          x: new Date(value.receiveDate).getTime(),
+          y: quantity*getPriceHistorical(value.asset),
+          x0: disposeDate,
+          y0: quantity*getPriceCurrent(value.asset)
+        }
       );
 
       newData.push(
@@ -57,19 +59,20 @@ export const Portfolio = () => {
           x: new Date(value.receiveDate).getTime(),
           y: quantity*getPriceHistorical(value.asset),
           x0: disposeDate,
-          y0: quantity*getPriceCurrent(value.asset)}
-      )
+          y0: quantity*getPriceCurrent(value.asset)
+        }
+      );
     });
     setData(newData);
   };
 
   useEffect(() => {
-    console.log("Generating graph data")
+    console.log("Generating graph data");
     formatChunksToGraphData();
 
-  }, [vm]);
+  }, [data]);
 
-  if(!data.length) return <> Loading </>
+  if(!data.length) return <> Loading </>;
 
   return (
     <XYPlot
