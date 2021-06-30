19bef8400bd5dc9afdeb5f251c7fb9d5e85e0717
bohendo <bohendo@pm.me>
2021-06-30 14:52:48 +0530

    plug ETH_PROVIDER env var into server

--- modules/server/src/prices.ts
+++ modules/server/src/prices.ts
@@ -30,6 +30,21 @@ pricesRouter.get("/:unit/:asset/:date", async (req, res) => {
   }
 });
 
+pricesRouter.post("/chunks/:unit", async (req, res) => {
+  const logAndSend = getLogAndSend(res);
+  const { unit } = req.params;
+  const { chunks } = req.body;
+  log.info(`Getting ${unit} prices for ${chunks.length} chunks`);
+  const prices = getPrices({ store, logger: log, unit: unit });
+  try {
+    const pricesJson = await prices.syncChunks(chunks);
+    logAndSend(pricesJson);
+  } catch (e) {
+    log.error(e.stack);
+    logAndSend(e.message, STATUS_YOUR_BAD);
+  }
+});
+
 pricesRouter.post("/:unit/:date", async (req, res) => {
   const logAndSend = getLogAndSend(res);
   const { unit, date } = req.params;
@@ -42,21 +57,6 @@ pricesRouter.post("/:unit/:date", async (req, res) => {
       output[asset] = await prices.syncPrice(date, asset);
     }
     res.json({ [date]: { [unit]: output } });
-  } catch (e) {
-    log.error(e.stack);
-    logAndSend(e.message, STATUS_YOUR_BAD);
-  }
-});
-
-pricesRouter.post("/chunks/:unit", async (req, res) => {
-  const logAndSend = getLogAndSend(res);
-  const { unit } = req.params;
-  const { chunks } = req.body;
-  log.info(`Getting ${unit} prices for ${chunks.length} chunks`);
-  const prices = getPrices({ store, logger: log, unit: unit });
-  try {
-    const pricesJson = await prices.syncChunks(chunks);
-    logAndSend(pricesJson);
   } catch (e) {
     log.error(e.stack);
     logAndSend(e.message, STATUS_YOUR_BAD);
