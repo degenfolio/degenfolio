2faa1e16cc7864e6effa66880b3f1879f134c322
bohendo <bohendo@pm.me>
2021-06-30 18:41:24 +0530

    use covalent to fetch prices

--- modules/server/src/prices.ts
+++ modules/server/src/prices.ts
@@ -1,6 +1,7 @@
 import { getLogger } from "@valuemachine/utils";
 import express from "express";
 import { getPrices } from "valuemachine";
+import axios from "axios";
 
 import { env } from "./env";
 import {
@@ -14,6 +15,19 @@ const log = getLogger(env.logLevel).child({
   module: "Prices",
 });
 
+const covalentUrl = "https://api.covalenthq.com/v1";
+const fetchPrice = async (date: string, unit: string, asset: string): Promise<string> => {
+  const url = `${covalentUrl}/pricing/historical/${unit}/${asset
+  }/?from=${date
+  }&to=${date
+  }&key=${env.covalentKey}`;
+  log.info(`GET ${url}`);
+  const response = await axios.get(url);
+  if (response.status !== 200) throw new Error(`Bad Status: ${response.status}`);
+  if (response.data.error) throw new Error(`Covalent Error: ${response.data.error_message}`);
+  return response.data.data.prices[0].price.toString();
+};
+
 export const pricesRouter = express.Router();
 
 pricesRouter.get("/:unit/:asset/:date", async (req, res) => {
@@ -22,8 +36,18 @@ pricesRouter.get("/:unit/:asset/:date", async (req, res) => {
   log.info(`Got request for ${unit} price of ${asset} on ${date}`);
   const prices = getPrices({ store, logger: log, unit: unit });
   try {
-    const price = await prices.syncPrice(date, asset);
-    logAndSend(price);
+    let price = prices.getPrice(date, asset);
+    if (price) {
+      return logAndSend(price);
+    } else {
+      price = await fetchPrice(date, unit, asset);
+      if (!price) {
+        return logAndSend(`Couldn't get price for unit=${unit} asset=${asset} date=${date}`);
+      } else {
+        prices.setPrice(price, date, asset, unit);
+        return logAndSend(price);
+      }
+    }
   } catch (e) {
     log.error(e.stack);
     logAndSend(e.message, STATUS_YOUR_BAD);
