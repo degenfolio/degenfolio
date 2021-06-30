ee25b8560625dcf09c372ba2a21f1969e019fc6b
bohendo <bohendo@pm.me>
2021-06-30 21:19:04 +0530

    add fallback

--- modules/server/src/prices.ts
+++ modules/server/src/prices.ts
@@ -15,17 +15,44 @@ const log = getLogger(env.logLevel).child({
   module: "Prices",
 });
 
-const covalentUrl = "https://api.covalenthq.com/v1";
-const fetchPrice = async (date: string, unit: string, asset: string): Promise<string> => {
+const fetchPrice = async (rawDate: string, unit: string, asset: string): Promise<string> => {
+  const covalentUrl = "https://api.covalenthq.com/v1";
+  const date = rawDate.includes("T") ? rawDate.split("T")[0] : rawDate;
   const url = `${covalentUrl}/pricing/historical/${unit}/${asset
   }/?from=${date
   }&to=${date
   }&key=${env.covalentKey}`;
   log.info(`GET ${url}`);
-  const response = await axios.get(url);
-  if (response.status !== 200) throw new Error(`Bad Status: ${response.status}`);
-  if (response.data.error) throw new Error(`Covalent Error: ${response.data.error_message}`);
-  return response.data.data.prices[0].price.toString();
+  try {
+    const response = await axios.get(url);
+    if (response.status !== 200) {
+      log.warn(`Bad Status: ${response.status}`);
+      return "";
+    } else if (response.data.error) {
+      log.warn(`Covalent Error: ${response.data.error_message}`);
+      return "";
+    } else {
+      return response.data.data.prices[0].price.toString();
+    }
+  } catch (e) {
+    log.warn(`${e.message}: ${
+      url.replace(covalentUrl, "covalent").replace(/&key=.*/, "")
+    }`);
+    return "";
+  }
+};
+
+const syncPrice = async (rawDate: string, unit: string, asset: string): Promise<string> => {
+  const prices = getPrices({ store, logger: log, unit: unit });
+  const date = rawDate.includes("T") ? rawDate.split("T")[0] : rawDate;
+  let price = prices.getPrice(date, asset);
+  if (price) { return price; }
+  // fetch price from Covalent
+  price = await fetchPrice(date, unit, asset);
+  if (price) { prices.setPrice(price, date, asset, unit); return price; }
+  price = await prices.syncPrice(date, asset);
+  if (price) { prices.setPrice(price, date, asset, unit); return price; }
+  throw new Error(`Couldn't get price of ${asset} on ${date}`);
 };
 
 export const pricesRouter = express.Router();
@@ -34,21 +61,30 @@ pricesRouter.get("/:unit/:asset/:date", async (req, res) => {
   const logAndSend = getLogAndSend(res);
   const { asset, date, unit } = req.params;
   log.info(`Got request for ${unit} price of ${asset} on ${date}`);
-  const prices = getPrices({ store, logger: log, unit: unit });
   try {
-    let price = prices.getPrice(date, asset);
-    if (price) return logAndSend(price);
-    price = await fetchPrice(date, unit, asset);
-    if (!price) return logAndSend(
-      `Couldn't get price unit=${unit} asset=${asset} date=${date}`,
-      STATUS_YOUR_BAD,
-    );
-    prices.setPrice(price, date, asset, unit);
-    return logAndSend(price);
+    return logAndSend(await syncPrice(date, unit, asset));
   } catch (e) {
-    log.error(e.stack);
+    log.error(e.message);
     logAndSend(e.message, STATUS_YOUR_BAD);
   }
+});
+
+pricesRouter.post("/:unit/:date", async (req, res) => {
+  const logAndSend = getLogAndSend(res);
+  const { unit, date } = req.params;
+  const { assets } = req.body;
+  if (!assets?.length) logAndSend("A list of assets is required", STATUS_YOUR_BAD);
+  log.info(`Got request for ${unit} prices on ${date} for assets: [${assets?.join(", ")}]`);
+  const output = {};
+  for (const asset of assets) {
+    try {
+      output[asset] = await syncPrice(date, unit, asset);
+    } catch (e) {
+      log.warn(e.message);
+    }
+  }
+  log.info(`Success, returning ${Object.keys(output).length} prices`);
+  logAndSend({ [date]: { [unit]: output } });
 });
 
 pricesRouter.post("/chunks/:unit", async (req, res) => {
@@ -61,37 +97,7 @@ pricesRouter.post("/chunks/:unit", async (req, res) => {
     const pricesJson = await prices.syncChunks(chunks);
     logAndSend(pricesJson);
   } catch (e) {
-    log.error(e.stack);
-    logAndSend(e.message, STATUS_YOUR_BAD);
-  }
-});
-
-pricesRouter.post("/:unit/:date", async (req, res) => {
-  const logAndSend = getLogAndSend(res);
-  const { unit, date } = req.params;
-  const { assets } = req.body;
-  log.info(`Got request for ${unit} prices on ${date} for assets: [${assets.join(", ")}]`);
-  const prices = getPrices({ store, logger: log, unit: unit });
-  const output = {};
-  try {
-    for (const asset of assets) {
-      let price = prices.getPrice(date, asset);
-      if (price) {
-        output[asset] = price;
-      } else {
-        price = await fetchPrice(date, unit, asset);
-        if (price) {
-          prices.setPrice(price, date, asset, unit);
-          output[asset] = price;
-        } else {
-          log.warn(`Couldn't get price: unit=${unit} asset=${asset} date=${date}`);
-        }
-      }
-    }
-    log.info(`Success, returning ${Object.keys(output).length} prices`);
-    res.status(200).json({ [date]: { [unit]: output } });
-  } catch (e) {
-    log.error(e.stack);
+    log.error(e.message);
     logAndSend(e.message, STATUS_YOUR_BAD);
   }
 });
