f7f3d3ebb9ff50f25a77f469bfa4087af4a77809
bohendo <bohendo@pm.me>
2021-07-03 15:46:12 +0530

    init harmony tests

---
+++ modules/adapters/src/harmony/harmonyData.test.ts
@@ -0,0 +1,50 @@
+import { ChainData } from "@valuemachine/types";
+import { getTransactionsError } from "@valuemachine/utils";
+
+import { Guards } from "../enums";
+
+import { getHarmonyData } from "./harmonyData";
+import {
+  expect,
+  getTestAddressBook,
+  testStore,
+  testLogger,
+  parseHarmonyTx,
+} from "./testUtils";
+
+const logger = testLogger.child({ module: `TestHarmony`,
+  level: "debug", // Uncomment to enable verbose logging
+});
+
+describe.only("Harmony Data", () => {
+  let harmonyData: ChainData;
+  const testAddress = "0xada083a3c06ee526F827b43695F2DcFf5C8C892B";
+  const addressBook = getTestAddressBook(testAddress);
+  beforeEach(() => {
+    harmonyData = getHarmonyData({
+      store: testStore,
+      logger,
+    });
+  });
+
+  it("should sync & parse a transaction", async () => {
+    const tx = await parseHarmonyTx({
+      selfAddress: testAddress,
+      hash: "0x292ec1392e758f33e77bd077334b413e5337f86698e99396befc123f8579f9fa",
+      logger,
+    });
+    logger.info(tx, `Got harmony transaction`);
+    expect(tx).to.be.ok;
+    expect(tx.sources).to.include(Guards.ONE);
+    expect(getTransactionsError([tx])).to.be.null;
+  });
+
+  it("should sync & parse an address book", async () => {
+    await harmonyData.syncAddressBook(addressBook);
+    const transactions = harmonyData.getTransactions(addressBook);
+    expect(transactions[0].sources).to.include(Guards.ONE);
+    expect(getTransactionsError(transactions)).to.be.null;
+  });
+
+});
+
